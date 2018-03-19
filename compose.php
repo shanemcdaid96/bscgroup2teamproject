4b4be72fcaac33d8c9c8a959b3bcd08b006177e3
Oliver Ferris <oliver@kdmireland.com>
2018-03-19 17:02:30 +0000

    New Compose UI

--- compose.php
+++ compose.php
@@ -11,107 +11,119 @@ $resultID = mysqli_query($connection,"SELECT name FROM users WHERE username Like
             $_SESSION['name'] = $name;
  
 // If session variable is not set it will redirect to login page
+
+
 if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
   header("location: index.php");
   exit;
 }
 
-        #PHP Source Code
-        require "phpspellcheck/include.php";
-
-        $mySpell = new SpellCheckButton();
-        $mySpell->InstallationPath = "/phpspellcheck/";
-        $mySpell->Fields = "ALL";
-        echo $mySpell->SpellImageButton();
-
-
-        $mySpell = new SpellAsYouType();
-        $mySpell->InstallationPath = "/phpspellcheck/";
-        $mySpell->Fields = "ALL";
-        echo $mySpell->Activate();
+    // $resultAttachmentID=mysqli_query($connection,"SELECT * FROM attachments WHERE IDemail=".$row['SubjectID']."");
+   // $rowAttachmentID=mysqli_fetch_assoc($resultAttachmentID);
 ?>
 
 
 
-<html lang="en" >
-
+<html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <title>Bsafe Email - Inbox</title>
-  <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
-
-  <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css'>
-<link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.2/css/font-awesome.min.css'>
-
-      <link rel="stylesheet" href="css/style.css">
-
   
+  <title>Bsafe Email | Compose </title>
+  <meta name="description" content="app, web app, responsive, admin dashboard, admin, flat, flat ui, ui kit, off screen nav" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+  <link rel="stylesheet" href="css/bootstrap.css" type="text/css" />
+  <link rel="stylesheet" href="css/animate.css" type="text/css" />
+  <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css" />
+  <link rel="stylesheet" href="css/font.css" type="text/css" cache="false" />
+  <link rel="stylesheet" href="css/plugin.css" type="text/css" />
+  <link rel="stylesheet" href="css/app.css" type="text/css" />
+    <link rel="stylesheet" type="text/css" href="css/table.css">
+
+
 </head>
-
 <body>
+  <section class="hbox stretch">
+    <!-- .aside -->
+    <aside class="bg-primary aside-sm" id="nav">
+      <section class="vbox">
+        <header class="dker nav-bar nav-bar-fixed-top">
+          <a class="btn btn-link visible-xs" data-toggle="class:nav-off-screen" data-target="#nav">
+            <i class="fa fa-bars"></i>
+          </a>
+          <a href="#" class="nav-brand" data-toggle="fullscreen">Bsafe Email</a>
+          <a class="btn btn-link visible-xs" data-toggle="class:show" data-target=".nav-user">
+            <i class="fa fa-comment-o"></i>
+          </a>
+        </header>
+        <section>
+          <!-- user -->
+          <div class="bg-success nav-user hidden-xs pos-rlt">
+            <div class="nav-avatar pos-rlt">
+            <h4 style="float: right; text-align: center;"><?php echo $name;?></h3>
+              <a href="#" class="thumb-sm avatar animated rollIn" data-toggle="dropdown">
+                <?php  $resultID = mysqli_query($connection,"SELECT ProfilePic FROM users WHERE username Like '".$_SESSION['username']."'"); 
+              $jfeta = mysqli_fetch_assoc($resultID);
+              // print($jfeta['ProfilePic']);
+              //'<img src=uploads/'.$rowAttachmentID['Filename'].'>'
+             print('<img src="Profile Images/'.$jfeta['ProfilePic'].'" width=75 height=75>');  ?>
 
-  <div class="navbar navbar-fixed-top navbar-default" role="navigation">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Welcome <?php echo $name ?></a>
-        </div>
-        <div class="collapse navbar-collapse">
-            <ul class="nav navbar-nav">
-                
-                
-                <li><a href="Logout.php"><i class="fas fa-sign-out-alt"></i> Logout</a></li>
+              </a>
 
+              
+            </div>
 
+          </div>
+          <!-- / user -->
+          <!-- nav -->
+          <nav class="nav-primary hidden-xs">
+            <ul class="nav">
+              <li class="">
+                <a href="inbox.php">
+                <i class="fas fa-inbox"></i>        
+                <span>Inbox</span>
+                </a>
+              </li>
 
+              <li class="">
+                <a href="sent.php">
+                <i class="fas fa-share-square"></i>
+                  <span>Sent</span>
+                </a>
+              </li>
+
+              <li class="active">
+                <a href="compose.php">
+                  <i class="fas fa-edit"></i>
+                  <span>Compose</span>
+                </a>
+              </li>
+
+              <li class="">
+                <a href="settings.php">
+                  <i class="fas fa-cogs"></i>
+                  <span>Settings</span>
+                </a>
+              </li>
+              <li>
             </ul>
-        </div>
-        <!-- /.nav-collapse -->
-    </div>
-    <!-- /.container -->
-</div>
-<!-- /.navbar -->
+          </nav>
+          <!-- / nav -->
 
-<div class="container-fluid">
-    <div class="row row-offcanvas row-offcanvas-left">
-       <div class="col-xs-2 col-sm-1 sidebar-offcanvas" id="sidebar" role="navigation"> 
-            <div class="sidebar-nav">
-                <ul class="nav">
-<li><a href="compose.php"><i class="fas fa-pencil-alt"></i> Compose</a></li>
-                    
-                    <li><a href=loggedin.php><i class="fas fa-inbox"></i> Inbox</a></li>
-                    <li><a href="sentPage.php"><i class="fas fa-share-square"></i> Sent</a></li>
-                    <li><a href="settings.php"><i class="fas fa-cog"></i> Settings</a></li>
-                   <!--<li><a href="#">Inbox</a></li>-->
-                    
-                </ul>
-            </div>            <!--/.well -->
-        </div>
-        <!--/span-->
+        </section>
+        <footer class="footer bg-gradient hidden-xs">
 
-        <div class="inboxMessages">
-
-<script type="text/javascript">
-    function custom_echo($x, $length)
-{
-  if(strlen($x)<=$length)
-  {
-    echo $x;
-  }
-  else
-  {
-    $y=substr($x,0,$length) . '...';
-    echo $y;
-  }
-}
-</script>
-<div id="settings">
+          <a href="LOGOUT" data-toggle="ajaxModal" class="btn btn-sm btn-link m-r-n-xs pull-right">
+            <i class="fa fa-power-off"></i><span> Logout</span>
+          </a>
+          <a href="#nav" data-toggle="class:nav-vertical" class="btn btn-sm btn-link m-l-n-sm">
+            <i class="fa fa-bars"></i>
+          </a>
+        </footer>
+      </section>
+    </aside>
+    <!-- /.aside -->
+    <!-- .vbox -->
+    <section id="content">
+<div id="compose" style="margin-left: 25px; margin-top: 50px;">
 <form action="insert.php" method="post" enctype="multipart/form-data">
     <h3>To:</h3>
     <input type="text" name="receiver"><input class="w-input-email"  placeholder="@bsafe-email.com" readonly>  
@@ -123,32 +135,27 @@ if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
     <input type="file" name="fileToUpload" id="fileToUpload">
     <br>
     <br>
-    <input type="submit">
+    <input type="submit" value="Send">
 </form>
 </div>
 
 
-</div>
-    </div>
-    <!--/row-->
 
-    <hr>
-
-    <footer>
-        <p>© Bsafe 2018</p>
-    </footer>
-
-</div>
-<!--/.container-->
-  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js'></script>
-
-  
-
-    <script  src="js/index.js"></script>
-
-
-
-
+      <a href="#" class="hide nav-off-screen-block" data-toggle="class:nav-off-screen" data-target="#nav"></a>
+    </section>
+    <!-- /.vbox -->
+  </section>
+	<script src="js/jquery.min.js"></script>
+  <!-- Bootstrap -->
+  <script src="js/bootstrap.js"></script>
+  <!-- Sparkline Chart -->
+  <script src="js/charts/sparkline/jquery.sparkline.min.js"></script>
+  <!-- App -->
+  <script src="js/app.js"></script>
+  <script src="js/app.plugin.js"></script>
+  <script src="js/app.data.js"></script>
+  <!-- Fa Icons -->
+  <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/solid.js" integrity="sha384-+Ga2s7YBbhOD6nie0DzrZpJes+b2K1xkpKxTFFcx59QmVPaSA8c7pycsNaFwUK6l" crossorigin="anonymous"></script>
+<script defer src="https://use.fontawesome.com/releases/v5.0.8/js/fontawesome.js" integrity="sha384-7ox8Q2yzO/uWircfojVuCQOZl+ZZBg2D2J5nkpLqzH1HY0C1dHlTKIbpRz/LG23c" crossorigin="anonymous"></script>  
 </body>
-
 </html>
